2fc6f0a7082b8df5649cffd55e96ac6f5b769586
Ivan <exemple@gmail.com>
2021-02-05 11:38:28 +0200

    movies

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '';)
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-    
-}
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотреных фильмов', ''),
+      b = prompt('На сколько оенете кго?', ''),
+      c = prompt('Один из последних просмотреных фильмов', ''),
+      d = prompt('На сколько оенете кго?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
